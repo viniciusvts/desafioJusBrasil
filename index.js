903a0dbff12083934c6d9c3efe4dc81f38dc4a31
Vinicius de Santana <vinicius.vts@gmail.com>
2019-03-21 20:24:35 -0300

    remoção de código não utilizado

--- index.js
+++ index.js
@@ -25,35 +25,3 @@ low(adapter)
             console.log("Estamos escutando na porta: "+port);
         })
     })
-
-/////////////////////////
-/*
-low(adapter)
-  .then(db => {
-    // Routes
-    // GET /posts/:id
-    app.get('/posts/:id', (req, res) => {
-      const post = db.get('posts')
-        .find({ id: req.params.id })
-        .value()
-
-      res.send(post)
-    })
-
-    // POST /posts
-    app.post('/posts', (req, res) => {
-      db.get('posts')
-        .push(req.body)
-        .last()
-        .assign({ id: Date.now().toString() })
-        .write()
-        .then(post => res.send(post))
-    })
-
-    // Set db default values
-    return db.defaults({ posts: [] }).write()
-  })
-  .then(() => {
-    app.listen(3000, () => console.log('listening on port 3000'))
-  })
-*/
